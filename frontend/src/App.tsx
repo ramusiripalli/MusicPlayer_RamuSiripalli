292ee726007bfe0b2b6e8e4c2f10660d8f84aea7
ramu siripalli <ramsiripalli2772@gmail.com>
2024-11-28 16:46:41 +0530

    Front End React Vite, Tailwind css and Shadcn UI installed and were Running
    Front End Vite, Tailwind css and Shadcn UI installed and were Running

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+
+
+  return (
+  <>
+  <div><h1 className="text-red-500 text-5xl">Hello</h1></div>
+  <Button variant={"destructive"}>This is a Button </Button>
+  </>
+  )
+}
+
+export default App
