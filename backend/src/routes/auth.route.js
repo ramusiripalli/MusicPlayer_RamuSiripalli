8a6d508154e4be423f7afe758d3226fda23d70bb
ramu siripalli <ramsiripalli2772@gmail.com>
2024-11-29 22:13:54 +0530

    update authcontroller
    update authcontroller

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,9 +1,9 @@
 import { Router } from 'express';
 
+import { authCallback } from '../controller/auth.controller.js';
+
 const router = Router();
 
-router.get('/',(req,res) => {
-    res.send('Auth Route with the get method');
-})
+router.post('/callback', authCallback);
 
 export default router;
