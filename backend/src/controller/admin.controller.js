e11ec3e451f2f8889b98626bf1b2daf0d4c73c41
ramu siripalli <ramsiripalli2772@gmail.com>
2024-12-10 21:34:58 +0530

    Created CreateAlbum and Delete Album controllers
    Created CreateAlbum and Delete Album controllers

--- backend/src/controller/admin.controller.js
+++ backend/src/controller/admin.controller.js
@@ -1,6 +1,6 @@
 import { Song } from "../models/song.model.js";
 import { Album } from "../models/album.model.js";
-import {cloudinary } from "../lib/cloudinaryy.js";
+import cloudinary from '../lib/cloudinaryy.js'
 
 const uploadToCloudinary = async (file) => {
 	try {
@@ -69,3 +69,40 @@ export const deleteSong = async(req,res,next) =>{
         next(error);
     }
 }
+
+
+export const createAlbum = async (req, res, next) => {
+	try {
+		const { title, artist, releaseYear } = req.body;
+		const { imageFile } = req.files;
+
+		const imageUrl = await uploadToCloudinary(imageFile);
+
+		const album = new Album({
+			title,
+			artist,
+			imageUrl,
+			releaseYear,
+		});
+
+		await album.save();
+
+		res.status(201).json(album);
+	} catch (error) {
+		console.log("Error in createAlbum", error);
+		next(error);
+	}
+};
+
+export const deleteAlbum = async (req, res, next) => {
+	try {
+		const { id } = req.params;
+		await Song.deleteMany({ albumId: id });
+		await Album.findByIdAndDelete(id);
+		res.status(200).json({ message: "Album deleted successfully" });
+	} catch (error) {
+		console.log("Error in deleteAlbum", error);
+		next(error);
+	}
+};
+
--- backend/src/routes/admin.route.js
+++ backend/src/routes/admin.route.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
-import { createSong,deleteSong } from '../controller/admin.controller.js';
+import { createSong,deleteSong,createAlbum, deleteAlbum } from '../controller/admin.controller.js';
 import { protectRoute, requireAdmin } from '../middleware/auth.middleware.js';
 
 const router = Router();
 
 router.post('/songs',protectRoute,requireAdmin,createSong);
 router.delete('/songs/:id',protectRoute,requireAdmin,deleteSong);
-
+router.post('/albums',protectRoute,requireAdmin,createAlbum);
+router.delete('/songs/:id',protectRoute,requireAdmin,deleteAlbum);
 
 export default router;
