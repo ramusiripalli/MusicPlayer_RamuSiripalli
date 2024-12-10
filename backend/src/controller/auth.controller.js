ea3483c7b89e82275e4b831af1115a15a7877e9a
ramu siripalli <ramsiripalli2772@gmail.com>
2024-12-10 17:18:03 +0530

    Added another cloudinaryy file getting error in first one
    Added another cloudinaryy file getting error in first one

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -1,5 +1,5 @@
 import { User } from '../models/user.model.js';
-export const authCallback = async (req,res) => {
+export const authCallback = async (req,res,next) => {
     try{
  const  {id,firstName,lastName, imageUrl} = req.body;
  const user = await User.findOne({clerkId: id});
@@ -16,6 +16,6 @@ export const authCallback = async (req,res) => {
  
     }catch(error){
  console.log("Error in authentication callback", error);
- res.status(500).json({message : "Internal Server Error", error});
+next(error);
     }
  }
