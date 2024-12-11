064501faffe5e8dd1e0c5526c7549ef3f9421f4b
ramu siripalli <ramsiripalli2772@gmail.com>
2024-12-11 17:09:00 +0530

    getMadeForYou and getTredingSongs controller updated
    getMadeForYou and getTredingSongs controller updated

--- backend/src/controller/song.controller.js
+++ backend/src/controller/song.controller.js
@@ -33,9 +33,47 @@ try {
 }
 
 export const getMadeForYouSongs = async(req,res,next) => {
-
+    try {
+        const songs = await Song.aggregate([
+            {
+                $sample:{size:4}
+            },
+            {
+                $project:{
+                    _id:1,
+                    title:1,
+                    artist:1,
+                    imageUrl:1,
+                    audioUrl:1
+                }
+            }
+        ]);
+    
+        res.status(200).json(songs);
+    } catch (error) {
+        next(error);
+    }
 }
 
 export const getTrendingSongs = async(req,res,next) => {
-
+    try {
+        const songs = await Song.aggregate([
+            {
+                $sample:{size:4}
+            },
+            {
+                $project:{
+                    _id:1,
+                    title:1,
+                    artist:1,
+                    imageUrl:1,
+                    audioUrl:1
+                }
+            }
+        ]);
+    
+        res.status(200).json(songs);
+    } catch (error) {
+        next(error);
+    }
 }
