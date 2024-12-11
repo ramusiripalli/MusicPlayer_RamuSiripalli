341ff3d76ef14c078f0861a31817383485a73afa
ramu siripalli <ramsiripalli2772@gmail.com>
2024-12-11 18:02:45 +0530

    Backend : getAllUsers , fetch all users expect mine by using $ne
    getAllUsers , fetch all users expect mine by using $ne

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,10 +1,9 @@
 import { Router } from 'express';
-//import { protectRoute } from '../middleware/auth.middleware.js';
+import { protectRoute } from '../middleware/auth.middleware.js';
+import { getAllUsers } from '../controller/user.controller.js';
 
 const router = Router();
 
-router.get('/like',(req,res) => {
-    res.send('User Route with the get method');
-})
+router.get('/',protectRoute,getAllUsers);
 
 export default router;
