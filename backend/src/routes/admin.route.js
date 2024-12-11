c6b5cd770b683f8f72fedf943dff8bc965f5c85a
ramu siripalli <ramsiripalli2772@gmail.com>
2024-12-11 14:19:00 +0530

    Added middleware for protect Route and require Admin in router.use
    export const checkAdmin = async (req, res, next) => {
    	res.status(200).json({ admin: true });
    };

--- backend/src/routes/admin.route.js
+++ backend/src/routes/admin.route.js
@@ -1,12 +1,13 @@
 import { Router } from 'express';
-import { createSong,deleteSong,createAlbum, deleteAlbum } from '../controller/admin.controller.js';
+import { createSong,deleteSong,createAlbum, deleteAlbum,checkAdmin } from '../controller/admin.controller.js';
 import { protectRoute, requireAdmin } from '../middleware/auth.middleware.js';
 
 const router = Router();
-
-router.post('/songs',protectRoute,requireAdmin,createSong);
-router.delete('/songs/:id',protectRoute,requireAdmin,deleteSong);
-router.post('/albums',protectRoute,requireAdmin,createAlbum);
-router.delete('/songs/:id',protectRoute,requireAdmin,deleteAlbum);
+router.use(protectRoute, requireAdmin);
+router.get('/check',checkAdmin);
+router.post('/songs',createSong);
+router.delete('/songs/:id',deleteSong);
+router.post('/albums',createAlbum);
+router.delete('/songs/:id',deleteAlbum);
 
 export default router;
