8718b2434c8a1530782e3874fcaef1fe60efb027
ramu siripalli <ramsiripalli2772@gmail.com>
2024-12-12 06:50:24 +0530

    HomePage and AuthCallbackPage
    FrontEnd : EsLint make it to off in eslint.config.js

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom"
-import HomePage from "./pages/home/HomePage"
-import AuthCallBackPage from "./pages/auth-callback/AuthCallBackPage"
+import HomePage from "./pages/home/HomePage.tsx"
+import AuthCallBackPage from "./pages/auth-callback/AuthCallBackPage.tsx"
 
 
 function App() {
