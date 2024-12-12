8eeb1b0e8374275867fba536c470ded3fe9d4d8f
ramu siripalli <ramsiripalli2772@gmail.com>
2024-12-12 07:27:56 +0530

    FrontEnd : Authprovider for every axiosInstance we are going to check the token , this means this is a authorized User
    FrontEnd : Authprovider for every axiosInstance we are going to check the token , this means this is a authorized User

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App.tsx';
 import { ClerkProvider } from '@clerk/clerk-react';
 import { BrowserRouter } from 'react-router-dom';
+import AuthProvider from './providers/AuthProvider.tsx';
 
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
@@ -17,9 +18,11 @@ console.log(PUBLISHABLE_KEY);
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
 <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+<AuthProvider>
 < BrowserRouter>
 <App />
 </BrowserRouter>
+</AuthProvider>
     </ClerkProvider>
   </StrictMode>,
 )
