954e8daff3633b5095e28570adeff41376378694
ramu siripalli <ramsiripalli2772@gmail.com>
2024-12-13 09:07:00 +0530

    Front End SSo-callback path
    Front End SSo-callback path

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom"
 import HomePage from "./pages/home/HomePage.tsx"
 import AuthCallBackPage from "./pages/auth-callback/AuthCallBackPage.tsx"
+import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react"
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
   <>
 <Routes>
   <Route path="/" element={<HomePage />} />
+  <Route path="/sso-callback" element={<AuthenticateWithRedirectCallback signUpForceRedirectUrl={"/auth-callback"} />} />
   <Route path="/auth-callback" element={<AuthCallBackPage />}/>
   <Route />
 </Routes>
