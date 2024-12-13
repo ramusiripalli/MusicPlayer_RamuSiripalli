75787108af7c4a2bbaef9bf6595e71b6f40dbba8
ramu siripalli <ramsiripalli2772@gmail.com>
2024-12-13 09:27:30 +0530

    FrontEnd: created MainLayout and Homepage was put inside
    FrontEnd: created MainLayout and Homepage was put inside

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,16 +2,20 @@ import { Route, Routes } from "react-router-dom"
 import HomePage from "./pages/home/HomePage.tsx"
 import AuthCallBackPage from "./pages/auth-callback/AuthCallBackPage.tsx"
 import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react"
+import MainLayout from "./layout/MainLayout.tsx"
 
 
 function App() {
   return (
   <>
 <Routes>
-  <Route path="/" element={<HomePage />} />
+  
   <Route path="/sso-callback" element={<AuthenticateWithRedirectCallback signUpForceRedirectUrl={"/auth-callback"} />} />
   <Route path="/auth-callback" element={<AuthCallBackPage />}/>
-  <Route />
+
+  <Route element={<MainLayout />}>
+  <Route path="/" element={<HomePage />} />
+  </Route>
 </Routes>
   </>
   )
