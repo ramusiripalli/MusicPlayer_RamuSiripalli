7a4cb8da452542553501846187bced4d0dd92aa7
ramu siripalli <ramsiripalli2772@gmail.com>
2024-12-13 10:36:33 +0530

    FrontEnd: MainComponent where we have resizeable panels
    FrontEnd: MainComponent where we have resizeable panels

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,19 +3,21 @@ import HomePage from "./pages/home/HomePage.tsx"
 import AuthCallBackPage from "./pages/auth-callback/AuthCallBackPage.tsx"
 import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react"
 import MainLayout from "./layout/MainLayout.tsx"
+import ChatPage from "./pages/chat/ChatPage.tsx"
 
 
 function App() {
   return (
   <>
 <Routes>
-  
+  <Route element={<MainLayout />}>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/chat" element={<ChatPage/>} />
+  </Route>
   <Route path="/sso-callback" element={<AuthenticateWithRedirectCallback signUpForceRedirectUrl={"/auth-callback"} />} />
   <Route path="/auth-callback" element={<AuthCallBackPage />}/>
 
-  <Route element={<MainLayout />}>
-  <Route path="/" element={<HomePage />} />
-  </Route>
+  
 </Routes>
   </>
   )
---
+++ frontend/src/layout/MainLayout.tsx
@@ -0,0 +1,34 @@
+import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "@/components/ui/resizable";
+import { Outlet } from "react-router-dom";
+
+
+const MainLayout = () => {
+    const isMobile=false;
+  return (
+    <div className="h-screen bg-black text-white flex flex-col">
+    <ResizablePanelGroup direction="horizontal" className="flex-1 flex  h-full overflow-hidden p-2">
+{/* left sidebar */}
+        <ResizablePanel defaultSize={20} minSize={isMobile?0:20} maxSize={30}>
+          
+        </ResizablePanel>
+        <ResizableHandle className="w-1 bg-white rounded-lg transition-colors" />
+{/* Main content main page */}
+        <ResizablePanel defaultSize={isMobile? 80: 60}>
+        <Outlet />
+        </ResizablePanel>
+        <ResizableHandle className="w-1 bg-white rounded-lg transition-colors" />
+{/* right sidebar for users */}
+        <ResizablePanel defaultSize={20} minSize={0} maxSize={25} collapsedSize={0}>
+            friends activity
+        </ResizablePanel>
+        
+        </ResizablePanelGroup>
+
+
+
+
+    </div>
+  )
+}
+
+export default MainLayout
