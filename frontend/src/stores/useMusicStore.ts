f5ebecbc41696b821f64fd3c55c5238d13573c75
ramu siripalli <ramsiripalli2772@gmail.com>
2024-12-16 16:36:23 +0530

    FrontEnd: Created useMusicStore statemanagement fetchAlbums and fetchAlbumsById updated
    FrontEnd: Created useMusicStore statemanagement fetchAlbums and fetchAlbumsById updated

--- frontend/src/stores/useMusicStore.ts
+++ frontend/src/stores/useMusicStore.ts
@@ -7,26 +7,42 @@ interface MusicStore{
     albums:Album[];
     isLoading:boolean;
     error: string | null;
+    currentAlbum: Album | null;
 
     fetchAlbums: () => Promise<void>;
+    fetchAlbumById: (id: string) => Promise<void>;
 }
 export const useMusicStore = create<MusicStore>((set)=>({
     albums:[],
     songs:[],
     isLoading:false,
     error:null,
-    fetchAlbums : async() => {
-set({isLoading: true,error:null});
-try {
-    const response = await axiosInstance.get('/albums');
-    set({albums: response.data });
-} catch (error : any) {
-    set({error: error.response.data.message});
-}
-finally{
-    set({isLoading: false});
-}
+    currentAlbum: null,
+    fetchAlbums : async() => 
+{
+        set({isLoading: true,error:null});
+    try {
+        const response = await axiosInstance.get('/albums');
+        set({albums: response.data });
+    } catch (error : any) {
+        set({error: error.response.data.message});
     }
+    finally{
+        set({isLoading: false});
+    }
+},
+
+    fetchAlbumById: async (id) => {
+        set({ isLoading: true, error: null });
+        try {
+            const response = await axiosInstance.get(`/albums/${id}`);
+            set({ currentAlbum: response.data });
+        } catch (error: any) {
+            set({ error: error.response.data.message });
+        } finally {
+            set({ isLoading: false });
+        }
+    },
 
 
 }))
