3a9b3b8c60e27d6a30f9aa44950e217811d82c02
ramu siripalli <ramsiripalli2772@gmail.com>
2024-12-16 08:52:06 +0530

    created index.ts for type safety for useMusicStore()
    created index.ts for type safety for useMusicStore()

--- frontend/src/layout/components/LeftSidebar.tsx
+++ frontend/src/layout/components/LeftSidebar.tsx
@@ -5,11 +5,17 @@ import { cn } from "@/lib/utils"
 import { SignedIn } from "@clerk/clerk-react"
 import { HomeIcon, Library, MessageCircle } from "lucide-react"
 import { Link } from "react-router-dom";
-
+import { useMusicStore  } from "@/stores/useMusicStore.ts";
+import { useEffect } from "react";
 
 
 const LeftSidebar = () => {
-    const isLoading = true;
+const { isLoading,albums,fetchAlbums} = useMusicStore();
+
+useEffect(() => {
+fetchAlbums();
+},[fetchAlbums]);
+console.log({albums});
   return (
     <div className="h-full flex flex-col gap-2">        
       <div className="rounded-lg bg-red-800 p-4">
@@ -40,7 +46,26 @@ const LeftSidebar = () => {
                 <div className="space-y-2">
                     {isLoading ? (
                         <PlaylistSkeleton />
-                    ) : ("Some Music")}
+                    ) : (
+                        albums.map((album) => (
+                            <Link
+                                to={`/albums/${album._id}`}
+                                key={album._id}
+                                className='p-2 hover:bg-zinc-800 rounded-md flex items-center gap-3 group cursor-pointer'
+                            >
+                                <img
+                                    src={album.imageUrl}
+                                    alt='Playlist img'
+                                    className='size-12 rounded-md flex-shrink-0 object-cover'
+                                />
+
+                                <div className='flex-1 min-w-0 hidden md:block'>
+                                    <p className='font-medium truncate'>{album.title}</p>
+                                    <p className='text-sm text-violet-400 truncate'>Album • {album.artist}</p>
+                                </div>
+                            </Link>
+                        ))
+                    )}
                 </div>
             </ScrollArea>
         </div>
