5d38ba64b57a339b7747b934db952c69fd80fbee
ramu siripalli <ramsiripalli2772@gmail.com>
2024-12-18 10:24:28 +0530

    FrontEnd: AuthProvider which takes cares of updating Tokens for every request
    FrontEnd: AuthProvider which takes cares of updating Tokens for every request

--- frontend/src/providers/AuthProvider.tsx
+++ frontend/src/providers/AuthProvider.tsx
@@ -1,48 +1,52 @@
 import { axiosInstance } from "@/lib/axios";
+import { useAuthStore } from "@/stores/useAuthStore";
+import { useChatStore } from "@/stores/useChatStore";
 import { useAuth } from "@clerk/clerk-react";
 import { Loader } from "lucide-react";
 import { useEffect, useState } from "react";
 
-const updateApiToken = (token : string | null) =>{
-    if(token){
-        axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-    }
-    else{
-        delete axiosInstance.defaults.headers.common['Authorization'];
-    }
-}
+const updateApiToken = (token: string | null) => {
+	if (token) axiosInstance.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+	else delete axiosInstance.defaults.headers.common["Authorization"];
+};
 
-const AuthProvider = ({ children }: { children: React.ReactNode }) =>{
-    const {getToken}= useAuth();
-    const[loading,setLoading] = useState(true);
+const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+	const { getToken, userId } = useAuth();
+	const [loading, setLoading] = useState(true);
+	const { checkAdminStatus } = useAuthStore();
+	const { initSocket, disconnectSocket } = useChatStore();
 
-    useEffect(()=>{
-        const initAuth = async() => {
-            try {
-                const token = await getToken();
-                updateApiToken(token);
-            } catch (error:any) {
-                updateApiToken(null);
-                console.log("Error in Auth Provider ")
-            }finally{
-                setLoading(false);
-            }
-        };
-        initAuth();
-    },[getToken]);
+	useEffect(() => {
+		const initAuth = async () => {
+			try {
+				const token = await getToken();
+				updateApiToken(token);
+				if (token) {
+					await checkAdminStatus();
+					// init socket
+					if (userId) initSocket(userId);
+				}
+			} catch (error: any) {
+				updateApiToken(null);
+				console.log("Error in auth provider", error);
+			} finally {
+				setLoading(false);
+			}
+		};
 
-    if (loading)
+		initAuth();
+
+		// clean up
+		return () => disconnectSocket();
+	}, [getToken, userId, checkAdminStatus, initSocket, disconnectSocket]);
+
+	if (loading)
 		return (
 			<div className='h-screen w-full flex items-center justify-center'>
-				<Loader className='size-25 text-emerald-500 animate-spin' />
+				<Loader className='size-8 text-emerald-500 animate-spin' />
 			</div>
 		);
 
-        return <>{children}</>;
-
-}
-
+	return <>{children}</>;
+};
 export default AuthProvider;
-
-
-
