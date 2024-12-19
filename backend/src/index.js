2020e5d7a564a4078f23ea6f478937ae16230add
ramu siripalli <ramsiripalli2772@gmail.com>
2024-12-19 15:46:39 +0530

    added crons to the package and added changes
    added crons to the package and added changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import { clerkMiddleware } from '@clerk/express';
 import fileUpload from 'express-fileupload';
 import path from 'path';
 import cors from 'cors';
+import cron from 'node-cron';
 import { connectDB } from './lib/db.js';
 import userRoutes from './routes/user.route.js';
 import adminRoutes from './routes/admin.route.js';
@@ -32,6 +33,21 @@ app.use(fileUpload({
         fileSize:10*1024*1024
     }
 }))
+
+const tempDir = path.join(process.cwd(), "tmp");
+cron.schedule("0 * * * *", () => {
+	if (fs.existsSync(tempDir)) {
+		fs.readdir(tempDir, (err, files) => {
+			if (err) {
+				console.log("error", err);
+				return;
+			}
+			for (const file of files) {
+				fs.unlink(path.join(tempDir, file), (err) => {});
+			}
+		});
+	}
+});
 app.use("/api/users",userRoutes);
 app.use("/api/admin", adminRoutes);
 app.use("/api/auth",authRoutes);
