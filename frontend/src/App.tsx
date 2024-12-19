b96ea2d01087d787bf01e26e70fe2a6be5e2c528
ramu siripalli <ramsiripalli2772@gmail.com>
2024-12-19 15:31:41 +0530

    Update App.tsx
    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/home/HomePage";
-import AuthCallbackPage from "./pages/auth-callback/AuthCallbackPage";
+import AuthCallbackPage from "./pages/auth-callback/AuthCallBackPage.tsx";
 import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
 import MainLayout from "./layout/MainLayout";
 import ChatPage from "./pages/chat/ChatPage";
 import AlbumPage from "./pages/album/AlbumPage";
-import AdminPage from "./pages/admin/AdminPage";
+import AdminPage from "./pages/admin/AdminPage.tsx";
 
 import { Toaster } from "react-hot-toast";
 import NotFoundPage from "./pages/404/NotFoundPage";
@@ -32,5 +32,4 @@ function App() {
 		</>
 	);
 }
-
 export default App;
